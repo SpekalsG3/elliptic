35f460970f6d2a4d1e5d312daf894605984f2d97
SpekalsG3 <karpick.kirill@gmail.com>
2024-03-14 21:50:42 +0300

    proper double_and_add fix handling point of infinity

--- src/curves/point.rs
+++ src/curves/point.rs
@@ -1,18 +1,26 @@
-use num_bigint::BigUint;
+use std::ops::Neg;
+use num_traits::One;
+use crate::field::field::Field;
 use crate::field::field_element::FieldElement;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Point<'a> {
     pub x: FieldElement<'a>,
     pub y: FieldElement<'a>,
+    pub z: FieldElement<'a>,
 }
 
 impl<'a> Point<'a> {
     pub fn is_infinity(&self) -> bool {
-        self.y.is_zero()
+        self.z.is_zero()
     }
-    pub fn is_identity(&self) -> bool {
-        self.y.value == BigUint::from(1_u8)
+
+    pub fn infinity(f: &'a Field) -> Self {
+        Self {
+            x: f.zero(),
+            y: f.one(),
+            z: f.zero(),
+        }
     }
 }
 
@@ -22,6 +30,7 @@ impl Neg for Point<'_> {
         Self {
             x: self.x,
             y: -self.y,
+            z: self.z,
         }
     }
 }
--- src/curves/weierstass.rs
+++ src/curves/weierstass.rs
@@ -36,16 +36,20 @@ impl<'a> WeierstrassCurve<'a> {
         ).map(|x| x.0)
     }
 
-    pub fn point_double(
+    fn point_double(
         &self,
         p1: Point<'a>,
     ) -> Point<'a> {
+        if p1.is_infinity() {
+            return p1;
+        }
         let three = FieldElement::new(p1.x.field, BigUint::from(3_u8));
         let slope = (three.clone() * p1.x.clone() * p1.x.clone() + self.a.clone()) / (p1.y.clone() + p1.y.clone());
         let x = slope.clone() * slope.clone() - p1.x.clone() - p1.x.clone();
         Point {
-            x: x.clone(),
+            z: x.field.one(),
             y: (three.clone() * p1.x.clone()) * slope.clone() - (slope.clone() ^ three.value) - p1.y,
+            x,
         }
     }
 
@@ -54,12 +58,15 @@ impl<'a> WeierstrassCurve<'a> {
         p1: Point<'a>,
         p2: Point<'a>,
     ) -> Point<'a> {
+        if p1.is_infinity() {
+            return p2;
+        }
+        if p2.is_infinity() {
+            return p1;
+        }
         if p1.x == p2.x {
             if p1.y == -p2.y {
-                return Point {
-                    x: p1.x.clone(),
-                    y: p1.x.field.zero(),
-                }
+                return Point::infinity(p1.x.field);
             }
             return self.point_double(p1);
         }
@@ -67,29 +74,33 @@ impl<'a> WeierstrassCurve<'a> {
         let slope = (p2.y - p1.y.clone()) / (p2.x.clone() - p1.x.clone());
         let x = slope.clone() * slope.clone() - p1.x.clone() - p2.x.clone();
         Point {
-            x: x.clone(),
+            z: x.field.one(),
             y: (p1.x.clone() + p1.x.clone() + p2.x) * slope.clone() - (slope ^ three.value) - p1.y,
+            x,
         }
     }
 
-    pub fn multi_by_double_and_add(
+    pub fn double_and_add(
         &self,
         k: FieldElement<'a>,
         p1: Point<'a>,
     ) -> Point<'a> {
-        let mut r0 = p1.clone();
+        let infinity = Point::infinity(k.field);
+        let mut r0 = infinity.clone();
         let mut r1 = p1.clone();
 
         for digit in k.value.iter_u32_digits() {
             for b in BitIter::from(digit).rev() {
                 if b {
                     r0 = self.point_add(r0.clone(), r1.clone());
+                } else {
+                    r0 = self.point_add(r0, infinity.clone());
                 }
                 r1 = self.point_add(r1.clone(), r1.clone())
             }
         }
 
-        self.point_add(r0, -p1)
+        r0
     }
 }
 
@@ -102,7 +113,7 @@ mod tests {
 
     #[test]
     fn test () {
-        let field = Field::new(BigUint::from(61_u32));
+        let field = Field::new(BigUint::from(61_u8));
         let e = WeierstrassCurve::new(
             field.get(BigUint::from(9_u8)),
             field.one(),
@@ -113,6 +124,7 @@ mod tests {
             Point {
                 x: x.clone(),
                 y: e.evaluate_y(x).unwrap(),
+                z: field.one(),
             }
         };
         assert_eq!(base.y, field.get(BigUint::from(7_u8)));
@@ -128,21 +140,24 @@ mod tests {
         assert_eq!(e.point_double(base.clone()), Point {
             x: field.get(BigUint::from(26_u8)),
             y: field.get(BigUint::from(50_u8)),
+            z: field.one(),
         });
         assert_eq!(naive_mul(3), Point {
             x: field.get(BigUint::from(27_u8)),
             y: field.get(BigUint::from(38_u8)),
+            z: field.one(),
         });
-        assert_eq!(naive_mul(5), e.multi_by_double_and_add(
+        assert_eq!(naive_mul(5), e.double_and_add(
             field.get(BigUint::from(5_u8)),
             base.clone(),
         ));
-        assert_eq!(e.multi_by_double_and_add(
+        assert_eq!(e.double_and_add(
             field.get(BigUint::from(142_u8)),
             base.clone(),
         ), Point {
             x: field.get(BigUint::from(48_u8)),
             y: field.get(BigUint::from(26_u8)),
+            z: field.one(),
         });
     }
 }
