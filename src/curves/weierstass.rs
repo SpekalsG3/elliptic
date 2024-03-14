349acb9dbf35616a119da1552b8f15ef0e8f9e97
SpekalsG3 <karpick.kirill@gmail.com>
2024-03-15 01:35:17 +0300

    implement montgomery_ladder multiplication

--- src/curves/weierstass.rs
+++ src/curves/weierstass.rs
@@ -10,6 +10,7 @@ pub struct WeierstrassCurve<'a> {
     a: FieldElement<'a>,
     #[allow(unused)]
     b: FieldElement<'a>,
+    order: BigUint,
 }
 
 // https://www.hyperelliptic.org/EFD/g1p/auto-shortw.html
@@ -17,11 +18,13 @@ impl<'a> WeierstrassCurve<'a> {
     pub fn new (
         a: FieldElement<'a>,
         b: FieldElement<'a>,
+        order: BigUint,
     ) -> Self {
         assert_eq!(a.field, b.field, "should be in the same field");
         Self {
             a,
             b,
+            order,
         }
     }
 
@@ -80,16 +83,68 @@ impl<'a> WeierstrassCurve<'a> {
         }
     }
 
-    pub fn double_and_add(
+    pub fn project_point_double(
         &self,
-        k: FieldElement<'a>,
         p1: Point<'a>,
     ) -> Point<'a> {
-        let infinity = Point::infinity(k.field);
+        let two = p1.x.field.get(BigUint::from(2_u8));
+        let three = p1.x.field.get(BigUint::from(3_u8));
+
+        let xx = p1.x.clone() * p1.x.clone();
+        let zz = p1.z.clone() * p1.z.clone();
+        let w = self.a.clone() * zz + three * xx.clone();
+        let s = two.clone() * p1.y.clone() * p1.z;
+        let ss = s.clone() * s.clone();
+        let sss = s.clone() * ss;
+        let r = p1.y * s.clone();
+        let rr = r.clone() * r.clone();
+        let xr = p1.x + r;
+        let b = xr.clone() * xr - xx - rr.clone();
+        let h = w.clone() * w.clone() - two.clone() * b.clone();
+
+        Point {
+            x: h.clone() * s,
+            y: w * (b - h) - two * rr,
+            z: sss,
+        }
+    }
+
+    pub fn project_point_add(
+        &self,
+        p1: Point<'a>,
+        p2: Point<'a>,
+    ) -> Point<'a> {
+        let two = p1.x.field.get(BigUint::from(2_u8));
+        let three = p1.x.field.get(BigUint::from(3_u8));
+
+        let u1 = p1.x * p2.z.clone();
+        let u2 = p2.x * p1.z.clone();
+        let s1 = p1.y * p2.z.clone();
+        let s2 = p2.y * p1.z.clone();
+        let w = p1.z * p2.z;
+        let p = u2.clone() - u1.clone();
+        let r = s2.clone() - s1.clone();
+        let pp = p.clone() * p.clone();
+        let ppp = pp.clone() * p.clone();
+        let rr = r.clone() * r.clone();
+
+        Point {
+            x: p *(-(u1.clone() + u2.clone()) * pp.clone() + w.clone() * rr.clone()),
+            y: (r.clone() * (-two.clone() * w.clone() * rr + three * (u1 + u2) * pp) - ppp.clone() * (s1 + s2)) / two,
+            z: w * ppp,
+        }
+    }
+
+    pub fn double_and_add(
+        &self,
+        k: BigUint,
+        p1: Point<'a>,
+    ) -> Point<'a> {
+        let infinity = Point::infinity(p1.x.field);
         let mut r0 = infinity.clone();
         let mut r1 = p1;
 
-        for digit in k.value.iter_u32_digits() {
+        for digit in k.iter_u32_digits() {
             for b in BitIter::from(digit).rev() {
                 if b {
                     r0 = self.point_add(r0.clone(), r1.clone());
@@ -97,6 +152,58 @@ impl<'a> WeierstrassCurve<'a> {
                     r0 = self.point_add(r0, infinity.clone()); // optional, against cache-timing attack
                 }
                 r1 = self.point_add(r1.clone(), r1.clone());
+            }
+        }
+
+        r0
+    }
+
+    pub fn montgomery_ladder(
+        &self,
+        k: BigUint,
+        p1: Point<'a>,
+    ) -> Point<'a> {
+        // find `n` where `curve order < 2 ^ n`
+        let size = u32::BITS as usize;
+        let mut bits = 0;
+        {
+            let mut order_iter = self.order.iter_u32_digits().rev();
+            if let Some(digit) = order_iter.next() {
+                let bit_iter = BitIter::from(digit).skip_while(|b| b == &false);
+                bits += bit_iter.count();
+            }
+            bits += order_iter.count() * size;
+        }
+
+        // convert k to keep `nth` bit always at one
+        // this lets us avoid setting r0 to point of infinity and handling it during operations
+        // by implicitly performing one iteration
+        let k = {
+            let n_2 = BigUint::from(1_u8) << bits;
+            let d = if k > n_2 {
+                (k - n_2.clone()) % self.order.clone()
+            } else {
+                self.order.clone() - (n_2.clone() - k) % self.order.clone()
+            };
+
+            n_2 + d
+        };
+
+        let mut r0 = p1.clone();
+        let mut r1 = self.project_point_double(p1.clone());
+
+        for digit in k.iter_u32_digits().rev() {
+            let count = bits % size;
+            bits -= count;
+
+            for b in BitIter::from(digit).skip(size - count) {
+                if b {
+                    r0 = self.project_point_add(r0.clone(), r1.clone());
+                    r1 = self.project_point_double(r1);
+                } else {
+                    r1 = self.project_point_add(r0.clone(), r1.clone());
+                    r0 = self.project_point_double(r0);
+                }
             }
         }
 
@@ -117,6 +224,7 @@ mod tests {
         let e = WeierstrassCurve::new(
             field.get(BigUint::from(9_u8)),
             field.one(),
+            BigUint::from(73_u8),
         );
 
         let base = {
@@ -148,16 +256,42 @@ mod tests {
             z: field.one(),
         });
         assert_eq!(naive_mul(5), e.double_and_add(
-            field.get(BigUint::from(5_u8)),
+            BigUint::from(5_u8),
             base.clone(),
         ));
         assert_eq!(e.double_and_add(
-            field.get(BigUint::from(142_u8)),
+            BigUint::from(142_u8),
             base.clone(),
         ), Point {
             x: field.get(BigUint::from(48_u8)),
             y: field.get(BigUint::from(26_u8)),
             z: field.one(),
         });
+        {
+            let p = e.project_point_double(base.clone()); // 2
+            assert_eq!(p.x.clone() / p.z.clone(), field.get(BigUint::from(26_u8)));
+            assert_eq!(p.y.clone() / p.z.clone(), field.get(BigUint::from(50_u8)));
+
+            let p = e.project_point_add(p, base.clone()); // 3
+            assert_eq!(p.x.clone() / p.z.clone(), field.get(BigUint::from(27_u8)));
+            assert_eq!(p.y.clone() / p.z.clone(), field.get(BigUint::from(38_u8)));
+
+            let p = e.project_point_add( // 5
+                e.project_point_double(p), // 6
+                -base.clone(), // -1
+            );
+            assert_eq!(p.x.clone() / p.z.clone(), field.get(BigUint::from(30_u8)));
+            assert_eq!(p.y.clone() / p.z.clone(), field.get(BigUint::from(59_u8)));
+        }
+
+        {
+            let p = e.montgomery_ladder(BigUint::from(5_u8), base.clone());
+            assert_eq!(p.x / p.z.clone(), field.get(BigUint::from(30_u8)));
+            assert_eq!(p.y / p.z.clone(), field.get(BigUint::from(59_u8)));
+
+            let p = e.montgomery_ladder(BigUint::from(142_u8), base.clone());
+            assert_eq!(p.x / p.z.clone(), field.get(BigUint::from(48_u8)));
+            assert_eq!(p.y / p.z.clone(), field.get(BigUint::from(26_u8)));
+        }
     }
 }
