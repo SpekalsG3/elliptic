6d16cf6e4f60865a14604df032661ff564c27ac9
SpekalsG3 <karpick.kirill@gmail.com>
2024-03-18 19:29:35 +0300

    implement generator method for field

--- src/field/field.rs
+++ src/field/field.rs
@@ -1,8 +1,10 @@
 use std::cmp::Ordering;
 use std::fmt::{Display, Formatter};
 use num_bigint::{BigInt, BigUint};
-use num_traits::Zero;
+use num_traits::{One, Zero};
+use rand::{RngCore, thread_rng};
 use crate::field::field_element::FieldElement;
+use crate::utils::s_tonelli::tonelli_shanks;
 use crate::utils::xgcd::u_xgcd;
 
 #[cfg(test)]
@@ -16,14 +18,6 @@ pub struct Field {
   pub order: BigUint,
 }
 
-impl From<&str> for Field {
-  fn from(value: &str) -> Self {
-    Self {
-      order: value.parse().unwrap(),
-    }
-  }
-}
-
 impl Display for Field {
   fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
     write!(f, "{}", self.order)
@@ -32,7 +26,6 @@ impl Display for Field {
 
 impl<'a> Field {
   pub fn new (order: BigUint) -> Field {
-    // assert_eq!(order, FIELD_PRIME, "Only 1+407*2^119 currently implemented");
     Field {
       order,
     }
@@ -64,6 +57,44 @@ impl<'a> Field {
         });
 
     FieldElement::new(self, res % self.order.clone())
+  }
+
+  pub fn generator(&self) -> Option<FieldElement<'_>> {
+    let mut fact = vec![];
+    let phi = self.order.clone() - BigUint::one();
+
+    let mut n = phi.clone();
+    let mut i = BigUint::from(2_u8);
+    while (i.clone() * i.clone()) <= n {
+      if (n.clone() % i.clone()).is_zero() {
+        fact.push(i.clone());
+        while (n.clone() % i.clone()).is_zero() {
+          n /= i.clone();
+        }
+      }
+
+      i += BigUint::one();
+    }
+    if n > BigUint::one() {
+      fact.push(n);
+    }
+
+    let mut res = self.get(BigUint::from(2_u8));
+    while res.value <= self.order {
+      let mut ok = true;
+      for f in &fact {
+        if (res.clone() ^ (phi.clone() / f)).is_one() {
+          ok = false;
+          break;
+        }
+      }
+
+      if ok {
+        return Some(res);
+      }
+      res.value += BigUint::one();
+    }
+    return None;
   }
 
   pub(crate) fn sub_mod (&self, a: BigUint, b: BigUint) -> BigUint {
@@ -138,6 +169,7 @@ impl<'a> Field {
 
 #[cfg(test)]
 mod tests {
+  use std::collections::HashMap;
   use super::*;
 
   #[test]
@@ -162,5 +194,37 @@ mod tests {
       field.add_mod(BigUint::from(20_u128), field.neg_mod(BigUint::from(19_u128))),
       BigUint::from(1_u8),
     );
+  }
+
+  #[test]
+  fn generator () {
+    let primes = [
+      BigUint::from(7_u16),
+      BigUint::from(193_u16),
+      BigUint::from(479_u16),
+      BigUint::from(631_u16),
+      BigUint::from(977_u16),
+    ];
+    for prime in primes {
+      let field = Field::new(prime);
+
+      let g = field.generator();
+      assert!(g.is_some());
+      let g = g.unwrap();
+
+      let mut map = HashMap::new();
+      map.insert(g.value.clone(), ());
+
+      let mut i = BigUint::from(2_u8);
+      while i < field.order {
+        let el = g.clone() ^ i.clone();
+
+        map.insert(el.value, ());
+
+        i += BigUint::one();
+      }
+
+      assert_eq!(BigUint::from(map.len()), field.order - BigUint::one(), "primitive root does not generate full field");
+    }
   }
 }
